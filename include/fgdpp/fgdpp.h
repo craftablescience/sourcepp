e9262ab4bcddb42883a97b68545880d6182c834a
craftablescience <me@craftablescience.info>
2024-04-09 22:05:49 -0400

    feat: clean up fgdpp

--- include/fgdpp/fgdpp.h
+++ include/fgdpp/fgdpp.h
@@ -1,62 +1,62 @@
 #pragma once
 
-
 #include <string>
+#include <string_view>
 #include <vector>
+
 #include "structs/entityproperties.h"
 
 namespace fgdpp {
 
-    class FGDParser {
+class FGD {
+	std::string rawFGDFile;
 
-        std::string rawFGDFile;
+	//TOKENIZER
+public:
+	enum TokenType {
+		COMMENT = 0,       // //
+		DEFINITION,        // @something
+		EQUALS,            // =
+		OPEN_BRACE,        // {
+		CLOSE_BRACE,       // }
+		OPEN_BRACKET,      // [
+		CLOSE_BRACKET,     // ]
+		OPEN_PARENTHESIS,  // (
+		CLOSE_PARENTHESIS, // )
+		COMMA,             // ,
+		STRING,            // "something"
+		PLUS,              // +
+		LITERAL,           // anything that isn't any of the other tokens.
+		COLUMN,            // :
+		NUMBER,            // numbers -200000 ... 0 ... 2000000
+	};
 
-        //TOKENIZER
-    public:
-        enum TokenType {
+	struct Token {
+		TokenType type;
+		Range range;
+		std::string_view string;
+		int line;
+		ParseError associatedError;
+	};
 
-            COMMENT = 0,       // //
-            DEFINITION,           // @something
-            EQUALS,               // =
-            OPEN_BRACE,           // {
-            CLOSE_BRACE,       // }
-            OPEN_BRACKET,       // [
-            CLOSE_BRACKET,       // ]
-            OPEN_PARENTHESIS,  // (
-            CLOSE_PARENTHESIS, // )
-            COMMA,               // ,
-            STRING,               // "something"
-            PLUS,               // +
-            LITERAL,           // anything that isn't any of the other tokens.
-            COLUMN,               // :
-            NUMBER,               // numbers -200000 ... 0 ... 2000000
+	std::vector<Token> tokenList;
 
-        };
+public:
+	FGD(std::string_view path, bool parseIncludes);
 
-        struct Token {
-            TokenType type;
-            Range range;
-            std::string_view string;
-            int line;
-            ParseError associatedError;
-        };
+private:
+	bool TokenizeFile();
 
-        std::vector<Token> tokenList;
-    public:
-        FGDParser(std::string_view path, bool parseIncludes);
-    private:
-        bool TokenizeFile();
+	//PARSER.
+public:
+	FGDFile FGDFileContents;
+	ParsingError parseError{ParseError::NO_ERROR, 0, {0, 0}};
 
-        //PARSER.
-    public:
+	bool ParseFile();
 
-        FGDFile FGDFileContents;
-        ParsingError parseError { NO_ERROR, 0, {0, 0}};
-
-        bool ParseFile();
 #ifdef FGDPP_UNIFIED_FGD
-        bool TagListDelimiter(std::vector<Token>::const_iterator &iter, TagList &tagList);
+	bool TagListDelimiter(std::vector<Token>::const_iterator& iter, TagList& tagList);
 #endif
-    };
+};
 
-}
+} // namespace fgdpp
