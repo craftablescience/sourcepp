06091c866332f82fa7f50af144ed1e82a95edca2
craftablescience <me@craftablescience.info>
2024-05-29 00:13:01 -0400

    wip(vtfpp): decode compressed image data

--- include/vtfpp/ImageFormats.h
+++ include/vtfpp/ImageFormats.h
@@ -62,47 +62,48 @@ namespace ImageFormatDetails {
 
 [[nodiscard]] constexpr int8_t red(ImageFormat format) {
 	switch (format) {
-		case ImageFormat::R32F:
-		case ImageFormat::RGB323232F:
-		case ImageFormat::RGBA32323232F:
+		using enum ImageFormat;
+		case R32F:
+		case RGB323232F:
+		case RGBA32323232F:
 			return 32;
-		case ImageFormat::RGBA16161616F:
-		case ImageFormat::RGBA16161616:
+		case RGBA16161616F:
+		case RGBA16161616:
 			return 16;
-		case ImageFormat::RGBA8888:
-		case ImageFormat::ABGR8888:
-		case ImageFormat::RGB888:
-		case ImageFormat::BGR888:
-		case ImageFormat::I8:
-		case ImageFormat::IA88:
-		case ImageFormat::P8:
-		case ImageFormat::RGB888_BLUESCREEN:
-		case ImageFormat::BGR888_BLUESCREEN:
-		case ImageFormat::ARGB8888:
-		case ImageFormat::BGRA8888:
-		case ImageFormat::BGRX8888:
-		case ImageFormat::UV88:
-		case ImageFormat::UVWQ8888:
-		case ImageFormat::UVLX8888:
+		case RGBA8888:
+		case ABGR8888:
+		case RGB888:
+		case BGR888:
+		case I8:
+		case IA88:
+		case P8:
+		case RGB888_BLUESCREEN:
+		case BGR888_BLUESCREEN:
+		case ARGB8888:
+		case BGRA8888:
+		case BGRX8888:
+		case UV88:
+		case UVWQ8888:
+		case UVLX8888:
 			return 8;
-		case ImageFormat::RGB565:
-		case ImageFormat::BGR565:
-		case ImageFormat::BGRX5551:
-		case ImageFormat::BGRA5551:
+		case RGB565:
+		case BGR565:
+		case BGRX5551:
+		case BGRA5551:
 			return 5;
-		case ImageFormat::BGRA4444:
+		case BGRA4444:
 			return 4;
-		case ImageFormat::A8:
-		case ImageFormat::EMPTY:
+		case A8:
+		case EMPTY:
 			return 0;
-		case ImageFormat::DXT1:
-		case ImageFormat::DXT3:
-		case ImageFormat::DXT5:
-		case ImageFormat::DXT1_ONE_BIT_ALPHA:
-		case ImageFormat::ATI2N:
-		case ImageFormat::ATI1N:
-		case ImageFormat::BC7:
-		case ImageFormat::BC6H:
+		case DXT1:
+		case DXT3:
+		case DXT5:
+		case DXT1_ONE_BIT_ALPHA:
+		case ATI2N:
+		case ATI1N:
+		case BC7:
+		case BC6H:
 			return -1;
 	}
 	return 0;
@@ -110,48 +111,49 @@ namespace ImageFormatDetails {
 
 [[nodiscard]] constexpr int8_t green(ImageFormat format) {
 	switch (format) {
-		case ImageFormat::RGB323232F:
-		case ImageFormat::RGBA32323232F:
+		using enum ImageFormat;
+		case RGB323232F:
+		case RGBA32323232F:
 			return 32;
-		case ImageFormat::RGBA16161616F:
-		case ImageFormat::RGBA16161616:
+		case RGBA16161616F:
+		case RGBA16161616:
 			return 16;
-		case ImageFormat::RGBA8888:
-		case ImageFormat::ABGR8888:
-		case ImageFormat::RGB888:
-		case ImageFormat::BGR888:
-		case ImageFormat::RGB888_BLUESCREEN:
-		case ImageFormat::BGR888_BLUESCREEN:
-		case ImageFormat::ARGB8888:
-		case ImageFormat::BGRA8888:
-		case ImageFormat::BGRX8888:
-		case ImageFormat::UV88:
-		case ImageFormat::UVWQ8888:
-		case ImageFormat::UVLX8888:
+		case RGBA8888:
+		case ABGR8888:
+		case RGB888:
+		case BGR888:
+		case RGB888_BLUESCREEN:
+		case BGR888_BLUESCREEN:
+		case ARGB8888:
+		case BGRA8888:
+		case BGRX8888:
+		case UV88:
+		case UVWQ8888:
+		case UVLX8888:
 			return 8;
-		case ImageFormat::RGB565:
-		case ImageFormat::BGR565:
+		case RGB565:
+		case BGR565:
 			return 6;
-		case ImageFormat::BGRX5551:
-		case ImageFormat::BGRA5551:
+		case BGRX5551:
+		case BGRA5551:
 			return 5;
-		case ImageFormat::BGRA4444:
+		case BGRA4444:
 			return 4;
-		case ImageFormat::I8:
-		case ImageFormat::IA88:
-		case ImageFormat::P8:
-		case ImageFormat::R32F:
-		case ImageFormat::A8:
-		case ImageFormat::EMPTY:
+		case I8:
+		case IA88:
+		case P8:
+		case R32F:
+		case A8:
+		case EMPTY:
 			return 0;
-		case ImageFormat::DXT1:
-		case ImageFormat::DXT3:
-		case ImageFormat::DXT5:
-		case ImageFormat::DXT1_ONE_BIT_ALPHA:
-		case ImageFormat::ATI2N:
-		case ImageFormat::ATI1N:
-		case ImageFormat::BC7:
-		case ImageFormat::BC6H:
+		case DXT1:
+		case DXT3:
+		case DXT5:
+		case DXT1_ONE_BIT_ALPHA:
+		case ATI2N:
+		case ATI1N:
+		case BC7:
+		case BC6H:
 			return -1;
 	}
 	return 0;
@@ -159,47 +161,48 @@ namespace ImageFormatDetails {
 
 [[nodiscard]] constexpr int8_t blue(ImageFormat format) {
 	switch (format) {
-		case ImageFormat::RGB323232F:
-		case ImageFormat::RGBA32323232F:
+		using enum ImageFormat;
+		case RGB323232F:
+		case RGBA32323232F:
 			return 32;
-		case ImageFormat::RGBA16161616F:
-		case ImageFormat::RGBA16161616:
+		case RGBA16161616F:
+		case RGBA16161616:
 			return 16;
-		case ImageFormat::RGBA8888:
-		case ImageFormat::ABGR8888:
-		case ImageFormat::RGB888:
-		case ImageFormat::BGR888:
-		case ImageFormat::RGB888_BLUESCREEN:
-		case ImageFormat::BGR888_BLUESCREEN:
-		case ImageFormat::ARGB8888:
-		case ImageFormat::BGRA8888:
-		case ImageFormat::BGRX8888:
-		case ImageFormat::UVWQ8888:
-		case ImageFormat::UVLX8888:
+		case RGBA8888:
+		case ABGR8888:
+		case RGB888:
+		case BGR888:
+		case RGB888_BLUESCREEN:
+		case BGR888_BLUESCREEN:
+		case ARGB8888:
+		case BGRA8888:
+		case BGRX8888:
+		case UVWQ8888:
+		case UVLX8888:
 			return 8;
-		case ImageFormat::RGB565:
-		case ImageFormat::BGR565:
-		case ImageFormat::BGRX5551:
-		case ImageFormat::BGRA5551:
+		case RGB565:
+		case BGR565:
+		case BGRX5551:
+		case BGRA5551:
 			return 5;
-		case ImageFormat::BGRA4444:
+		case BGRA4444:
 			return 4;
-		case ImageFormat::I8:
-		case ImageFormat::IA88:
-		case ImageFormat::P8:
-		case ImageFormat::UV88:
-		case ImageFormat::R32F:
-		case ImageFormat::A8:
-		case ImageFormat::EMPTY:
+		case I8:
+		case IA88:
+		case P8:
+		case UV88:
+		case R32F:
+		case A8:
+		case EMPTY:
 			return 0;
-		case ImageFormat::DXT1:
-		case ImageFormat::DXT3:
-		case ImageFormat::DXT5:
-		case ImageFormat::DXT1_ONE_BIT_ALPHA:
-		case ImageFormat::ATI2N:
-		case ImageFormat::ATI1N:
-		case ImageFormat::BC7:
-		case ImageFormat::BC6H:
+		case DXT1:
+		case DXT3:
+		case DXT5:
+		case DXT1_ONE_BIT_ALPHA:
+		case ATI2N:
+		case ATI1N:
+		case BC7:
+		case BC6H:
 			return -1;
 	}
 	return 0;
@@ -207,47 +210,48 @@ namespace ImageFormatDetails {
 
 [[nodiscard]] constexpr int8_t alpha(ImageFormat format) {
 	switch (format) {
-		case ImageFormat::RGBA32323232F:
+		using enum ImageFormat;
+		case RGBA32323232F:
 			return 32;
-		case ImageFormat::RGBA16161616F:
-		case ImageFormat::RGBA16161616:
+		case RGBA16161616F:
+		case RGBA16161616:
 			return 16;
-		case ImageFormat::RGBA8888:
-		case ImageFormat::ABGR8888:
-		case ImageFormat::IA88:
-		case ImageFormat::ARGB8888:
-		case ImageFormat::BGRA8888:
-		case ImageFormat::BGRX8888:
-		case ImageFormat::UVWQ8888:
-		case ImageFormat::UVLX8888:
+		case RGBA8888:
+		case ABGR8888:
+		case IA88:
+		case ARGB8888:
+		case BGRA8888:
+		case BGRX8888:
+		case UVWQ8888:
+		case UVLX8888:
 			return 8;
-		case ImageFormat::BGRA4444:
+		case BGRA4444:
 			return 4;
-		case ImageFormat::BGRX5551:
-		case ImageFormat::BGRA5551:
+		case BGRX5551:
+		case BGRA5551:
 			return 1;
-		case ImageFormat::RGB888:
-		case ImageFormat::BGR888:
-		case ImageFormat::P8:
-		case ImageFormat::I8:
-		case ImageFormat::RGB888_BLUESCREEN:
-		case ImageFormat::BGR888_BLUESCREEN:
-		case ImageFormat::UV88:
-		case ImageFormat::RGB565:
-		case ImageFormat::BGR565:
-		case ImageFormat::R32F:
-		case ImageFormat::RGB323232F:
-		case ImageFormat::A8:
-		case ImageFormat::EMPTY:
+		case RGB888:
+		case BGR888:
+		case P8:
+		case I8:
+		case RGB888_BLUESCREEN:
+		case BGR888_BLUESCREEN:
+		case UV88:
+		case RGB565:
+		case BGR565:
+		case R32F:
+		case RGB323232F:
+		case A8:
+		case EMPTY:
 			return 0;
-		case ImageFormat::DXT1:
-		case ImageFormat::DXT3:
-		case ImageFormat::DXT5:
-		case ImageFormat::DXT1_ONE_BIT_ALPHA:
-		case ImageFormat::ATI2N:
-		case ImageFormat::ATI1N:
-		case ImageFormat::BC7:
-		case ImageFormat::BC6H:
+		case DXT1:
+		case DXT3:
+		case DXT5:
+		case DXT1_ONE_BIT_ALPHA:
+		case ATI2N:
+		case ATI1N:
+		case BC7:
+		case BC6H:
 			return -1;
 	}
 	return 0;
