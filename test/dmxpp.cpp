09077d1c6c64f02982e0615c837430f13ed946e6
craftablescience <me@craftablescience.info>
2024-06-02 00:29:46 -0400

    chore: move createFile to test helpers

--- test/dmxpp.cpp
+++ test/dmxpp.cpp
@@ -8,21 +8,21 @@ using namespace dmxpp;
 
 // v1 and v2 are identical afaik...
 TEST(dmxpp, v2_read) {
-	DMX dmx{::readFileToBuffer(ASSET_ROOT "dmxpp/binary/v2.dmx")};
+	DMX dmx{::readFile(ASSET_ROOT "dmxpp/binary/v2.dmx")};
 	ASSERT_TRUE(dmx);
 }
 
 TEST(dmxpp, v3_read) {
-	DMX dmx{::readFileToBuffer(ASSET_ROOT "dmxpp/binary/v3.dmx")};
+	DMX dmx{::readFile(ASSET_ROOT "dmxpp/binary/v3.dmx")};
 	ASSERT_TRUE(dmx);
 }
 
 TEST(dmxpp, v4_read) {
-	DMX dmx{::readFileToBuffer(ASSET_ROOT "dmxpp/binary/v4.dmx")};
+	DMX dmx{::readFile(ASSET_ROOT "dmxpp/binary/v4.dmx")};
 	ASSERT_TRUE(dmx);
 }
 
 TEST(dmxpp, v5_read) {
-	DMX dmx{::readFileToBuffer(ASSET_ROOT "dmxpp/binary/v5.dmx")};
+	DMX dmx{::readFile(ASSET_ROOT "dmxpp/binary/v5.dmx")};
 	ASSERT_TRUE(dmx);
 }
--- test/studiomodelpp.cpp
+++ test/studiomodelpp.cpp
@@ -8,24 +8,24 @@ using namespace studiomodelpp;
 
 TEST(studiomodelpp, v44_read) {
 	StudioModel model;
-	bool opened = model.open(::readFileToBuffer(ASSET_ROOT "studiomodelpp/v44.mdl"),
-	                         ::readFileToBuffer(ASSET_ROOT "studiomodelpp/v44.vtx"),
-	                         ::readFileToBuffer(ASSET_ROOT "studiomodelpp/v44.vvd"));
+	bool opened = model.open(::readFile(ASSET_ROOT "studiomodelpp/v44.mdl"),
+	                         ::readFile(ASSET_ROOT "studiomodelpp/v44.vtx"),
+	                         ::readFile(ASSET_ROOT "studiomodelpp/v44.vvd"));
 	ASSERT_TRUE(opened);
 }
 
 TEST(studiomodelpp, v47_read) {
 	StudioModel model;
-	bool opened = model.open(::readFileToBuffer(ASSET_ROOT "studiomodelpp/v47.mdl"),
-	                         ::readFileToBuffer(ASSET_ROOT "studiomodelpp/v47.vtx"),
-	                         ::readFileToBuffer(ASSET_ROOT "studiomodelpp/v47.vvd"));
+	bool opened = model.open(::readFile(ASSET_ROOT "studiomodelpp/v47.mdl"),
+	                         ::readFile(ASSET_ROOT "studiomodelpp/v47.vtx"),
+	                         ::readFile(ASSET_ROOT "studiomodelpp/v47.vvd"));
 	ASSERT_TRUE(opened);
 }
 
 TEST(studiomodelpp, v49_read) {
 	StudioModel model;
-	bool opened = model.open(::readFileToBuffer(ASSET_ROOT "studiomodelpp/v49.mdl"),
-	                         ::readFileToBuffer(ASSET_ROOT "studiomodelpp/v49.vtx"),
-	                         ::readFileToBuffer(ASSET_ROOT "studiomodelpp/v49.vvd"));
+	bool opened = model.open(::readFile(ASSET_ROOT "studiomodelpp/v49.mdl"),
+	                         ::readFile(ASSET_ROOT "studiomodelpp/v49.vtx"),
+	                         ::readFile(ASSET_ROOT "studiomodelpp/v49.vvd"));
 	ASSERT_TRUE(opened);
 }
