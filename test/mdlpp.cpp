70be5098d4c0f85a8d5072ab1a7046c6f3b0b6fd
craftablescience <me@craftablescience.info>
2024-06-17 19:20:38 -0400

    chore(vmfpp): delete it, superseded by kvpp

--- test/mdlpp.cpp
+++ test/mdlpp.cpp
@@ -8,24 +8,24 @@ using namespace mdlpp;
 
 TEST(mdlpp, v44_read) {
 	StudioModel model;
-	bool opened = model.open(::readFile(ASSET_ROOT "studiomodelpp/v44.mdl"),
-	                         ::readFile(ASSET_ROOT "studiomodelpp/v44.vtx"),
-	                         ::readFile(ASSET_ROOT "studiomodelpp/v44.vvd"));
+	bool opened = model.open(::readFile(ASSET_ROOT "mdlpp/v44.mdl"),
+	                         ::readFile(ASSET_ROOT "mdlpp/v44.vtx"),
+	                         ::readFile(ASSET_ROOT "mdlpp/v44.vvd"));
 	ASSERT_TRUE(opened);
 }
 
 TEST(mdlpp, v47_read) {
 	StudioModel model;
-	bool opened = model.open(::readFile(ASSET_ROOT "studiomodelpp/v47.mdl"),
-	                         ::readFile(ASSET_ROOT "studiomodelpp/v47.vtx"),
-	                         ::readFile(ASSET_ROOT "studiomodelpp/v47.vvd"));
+	bool opened = model.open(::readFile(ASSET_ROOT "mdlpp/v47.mdl"),
+	                         ::readFile(ASSET_ROOT "mdlpp/v47.vtx"),
+	                         ::readFile(ASSET_ROOT "mdlpp/v47.vvd"));
 	ASSERT_TRUE(opened);
 }
 
 TEST(mdlpp, v49_read) {
 	StudioModel model;
-	bool opened = model.open(::readFile(ASSET_ROOT "studiomodelpp/v49.mdl"),
-	                         ::readFile(ASSET_ROOT "studiomodelpp/v49.vtx"),
-	                         ::readFile(ASSET_ROOT "studiomodelpp/v49.vvd"));
+	bool opened = model.open(::readFile(ASSET_ROOT "mdlpp/v49.mdl"),
+	                         ::readFile(ASSET_ROOT "mdlpp/v49.vtx"),
+	                         ::readFile(ASSET_ROOT "mdlpp/v49.vvd"));
 	ASSERT_TRUE(opened);
 }
