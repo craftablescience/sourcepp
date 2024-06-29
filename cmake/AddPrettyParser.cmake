2663d2f5bd4cbc5828e21015691e95e5a0bb0750
craftablescience <me@craftablescience.info>
2024-06-29 19:06:03 -0400

    fix(cmake): add test files in add_sourcepp_library, not add_pretty_parser

--- cmake/AddPrettyParser.cmake
+++ cmake/AddPrettyParser.cmake
@@ -1,6 +1,6 @@
 # Add a new parser library
 function(add_pretty_parser TARGET)
-    cmake_parse_arguments(PARSE_ARGV 1 OPTIONS "C;NO_TEST" "" "DEPS;SOURCES")
+    cmake_parse_arguments(PARSE_ARGV 1 OPTIONS "C" "" "DEPS;SOURCES")
 
     if(OPTIONS_C)
         add_library(${TARGET}c SHARED ${${PROJECT_NAME}c_SOURCES} ${OPTIONS_SOURCES})
@@ -31,15 +31,5 @@ function(add_pretty_parser TARGET)
         if(UNIX OR MINGW)
             target_compile_options(${TARGET} PRIVATE -O2 -fno-omit-frame-pointer)
         endif()
-    endif()
-
-    # Add tests
-    if(NOT OPTIONS_C AND NOT OPTIONS_NO_TEST AND SOURCEPP_BUILD_TESTS)
-        list(APPEND ${SOURCEPP_TEST_NAME}_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/test/${TARGET}.cpp")
-        list(APPEND ${SOURCEPP_TEST_NAME}_DEPS ${TARGET})
-
-        # Gross hack
-        set(${SOURCEPP_TEST_NAME}_SOURCES ${${SOURCEPP_TEST_NAME}_SOURCES} PARENT_SCOPE)
-        set(${SOURCEPP_TEST_NAME}_DEPS ${${SOURCEPP_TEST_NAME}_DEPS} PARENT_SCOPE)
     endif()
 endfunction()
--- cmake/AddSourcePPLibrary.cmake
+++ cmake/AddSourcePPLibrary.cmake
@@ -1,19 +1,27 @@
 function(add_sourcepp_library TARGET)
-    cmake_parse_arguments(PARSE_ARGV 1 OPTIONS "C;CSHARP" "" "")
+    cmake_parse_arguments(PARSE_ARGV 1 OPTIONS "C;CSHARP;NO_TEST" "" "")
     string(TOUPPER ${TARGET} TARGET_UPPER)
     if(SOURCEPP_USE_${TARGET_UPPER})
+        # Add C++
         include("${CMAKE_CURRENT_SOURCE_DIR}/src/${TARGET}/_${TARGET}.cmake")
+
+        # Add C
         if(SOURCEPP_BUILD_C_WRAPPERS AND OPTIONS_C)
             include("${CMAKE_CURRENT_SOURCE_DIR}/lang/c/src/${TARGET}c/_${TARGET}c.cmake")
         endif()
+
+        # Add C#
         if(OPTIONS_CSHARP)
-            configure_file("${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/sourcepp/Buffer.cs.in"     "${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/${TARGET}/Buffer.cs")
-            configure_file("${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/sourcepp/String.cs.in"     "${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/${TARGET}/String.cs")
-            configure_file("${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/sourcepp/TARGET.csproj.in" "${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/${TARGET}/${TARGET}.csproj")
+            configure_file("${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/src/sourcepp/Buffer.cs.in"     "${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/src/${TARGET}/Buffer.cs")
+            configure_file("${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/src/sourcepp/String.cs.in"     "${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/src/${TARGET}/String.cs")
+            configure_file("${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/src/sourcepp/TARGET.csproj.in" "${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/src/${TARGET}/${TARGET}.csproj")
         endif()
 
-        # Gross hack
-        set(${SOURCEPP_TEST_NAME}_SOURCES ${${SOURCEPP_TEST_NAME}_SOURCES} PARENT_SCOPE)
-        set(${SOURCEPP_TEST_NAME}_DEPS ${${SOURCEPP_TEST_NAME}_DEPS} PARENT_SCOPE)
+        # Add tests
+        if(NOT OPTIONS_NO_TEST AND SOURCEPP_BUILD_TESTS)
+            list(APPEND ${SOURCEPP_TEST_NAME}_DEPS ${TARGET})
+            list(APPEND ${SOURCEPP_TEST_NAME}_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/test/${TARGET}.cpp")
+            return(PROPAGATE ${SOURCEPP_TEST_NAME}_DEPS ${SOURCEPP_TEST_NAME}_SOURCES)
+        endif()
     endif()
 endfunction()
