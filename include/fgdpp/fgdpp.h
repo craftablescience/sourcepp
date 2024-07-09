e57759cf636de23f239b348f71165c445aaa4dd0
craftablescience <me@craftablescience.info>
2024-07-09 00:50:28 -0400

    feat(fgdpp): replace parser with faster library

--- include/fgdpp/fgdpp.h
+++ include/fgdpp/fgdpp.h
@@ -1,62 +1,116 @@
 #pragma once
 
+#include <list>
 #include <string>
 #include <string_view>
+#include <unordered_map>
 #include <vector>
 
-#include "structs/EntityProperties.h"
+#include <sourcepp/math/Vector.h>
+
+class BufferStream;
+class BufferStreamReadOnly;
 
 namespace fgdpp {
 
 class FGD {
-	std::string rawFGDFile;
-
-	//TOKENIZER
 public:
-	enum TokenType {
-		COMMENT = 0,       // //
-		DEFINITION,        // @something
-		EQUALS,            // =
-		OPEN_BRACE,        // {
-		CLOSE_BRACE,       // }
-		OPEN_BRACKET,      // [
-		CLOSE_BRACKET,     // ]
-		OPEN_PARENTHESIS,  // (
-		CLOSE_PARENTHESIS, // )
-		COMMA,             // ,
-		STRING,            // "something"
-		PLUS,              // +
-		LITERAL,           // anything that isn't any of the other tokens.
-		COLUMN,            // :
-		NUMBER,            // numbers -200000 ... 0 ... 2000000
+	struct Entity {
+		struct ClassProperty {
+			std::string_view name;
+			std::string_view arguments;
+		};
+
+		struct Field {
+			std::string_view name;
+			std::string_view valueType;
+			std::string_view displayName;
+			std::string_view valueDefault;
+			std::string_view description;
+		};
+
+		struct FieldChoices {
+			struct Choice {
+				std::string_view value;
+				std::string_view displayName;
+			};
+
+			std::string_view name;
+			std::string_view displayName;
+			std::string_view valueDefault;
+			std::string_view description;
+			std::vector<Choice> choices;
+		};
+
+		struct FieldFlags {
+			struct Flag {
+				std::string_view value;
+				std::string_view displayName;
+				std::string_view enabledByDefault;
+				std::string_view description;
+			};
+
+			std::string_view name;
+			std::vector<Flag> flags;
+		};
+
+		struct IO {
+			std::string_view name;
+			std::string_view valueType;
+			std::string_view description;
+		};
+
+		std::string_view classType;
+		std::vector<ClassProperty> classProperties;
+
+		std::string_view description;
+
+		std::vector<Field> fields;
+		std::vector<FieldChoices> fieldsWithChoices;
+		std::vector<FieldFlags> fieldsWithFlags;
+		std::vector<IO> inputs;
+		std::vector<IO> outputs;
 	};
 
-	struct Token {
-		TokenType type;
-		Range range;
-		std::string_view string;
-		int line;
-		ParseError associatedError;
+	struct AutoVisGroup {
+		std::string_view parentName;
+		std::string_view name;
+		std::vector<std::string_view> entities;
 	};
 
-	std::vector<Token> tokenList;
+	FGD() = default;
 
-public:
-	FGD(std::string_view path, bool parseIncludes);
+	explicit FGD(const std::string& fgdPath);
+
+	/**
+	 * Can be called multiple times in succession to load multiple FGD files.
+	 * The FGD file data will be merged with previously loaded data.
+	 * @param fgdPath The path to the FGD to load
+	 */
+	void load(const std::string& fgdPath);
+
+	[[nodiscard]] int getVersion() const;
+
+	[[nodiscard]] sourcepp::math::Vec2i getMapSize() const;
+
+	[[nodiscard]] const std::unordered_map<std::string_view, Entity>& getEntities() const;
+
+	[[nodiscard]] const std::vector<std::string_view>& getMaterialExclusionDirs() const;
+
+	[[nodiscard]] const std::vector<AutoVisGroup>& getAutoVisGroups() const;
+
+	explicit operator bool() const;
 
 private:
-	bool TokenizeFile();
+	void readEntities(BufferStreamReadOnly& stream, const std::string& path, std::vector<std::string>& seenPaths);
 
-	//PARSER.
-public:
-	FGDFile FGDFileContents;
-	ParsingError parseError{ParseError::NO_ERROR, 0, {0, 0}};
+	std::list<std::string> backingData;
 
-	bool ParseFile();
-
-#ifdef FGDPP_UNIFIED_FGD
-	bool TagListDelimiter(std::vector<Token>::const_iterator& iter, TagList& tagList);
-#endif
+	int version = 0;
+	sourcepp::math::Vec2i mapSize{};
+	std::unordered_map<std::string_view, Entity> entities;
+	std::vector<std::string_view> materialExclusionDirs;
+	std::vector<AutoVisGroup> autoVisGroups;
 };
 
 } // namespace fgdpp
