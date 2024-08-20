560da0c93bf7efc93416315e03ac48c213d0e107
craftablescience <me@craftablescience.info>
2024-08-20 02:08:15 -0400

    feat: add sourcepp::<name> target alias to all libraries in cmake

--- src/steampp/_steampp.cmake
+++ src/steampp/_steampp.cmake
@@ -1,5 +1,5 @@
 add_pretty_parser(steampp
-        DEPS kvpp
+        DEPS sourcepp::kvpp
         SOURCES
         "${CMAKE_CURRENT_SOURCE_DIR}/include/steampp/steampp.h"
         "${CMAKE_CURRENT_LIST_DIR}/steampp.cpp")
