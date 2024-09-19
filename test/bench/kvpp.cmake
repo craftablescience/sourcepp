a5d7e1e7872dc030ee647f8cef05034445f591f3
craftablescience <me@craftablescience.info>
2024-09-19 15:32:00 -0400

    fix(bench): make sure external libraries are getting the same optimization flags set

--- test/bench/kvpp.cmake
+++ test/bench/kvpp.cmake
@@ -3,5 +3,6 @@ FetchContent_Declare(
         GIT_REPOSITORY https://github.com/ozxybox/SpeedyKeyV.git
         GIT_TAG origin/master)
 FetchContent_MakeAvailable(keyvalues)
+target_optimize(keyvalues)
 
 target_link_libraries(${TARGET}_bench PUBLIC keyvalues)
--- test/bench/vtfpp.cmake
+++ test/bench/vtfpp.cmake
@@ -3,9 +3,7 @@ FetchContent_Declare(
         GIT_REPOSITORY https://github.com/StrataSource/VTFLib.git
         GIT_TAG origin/master)
 FetchContent_MakeAvailable(vtflib)
-set_target_properties(vtflib PROPERTIES
-        LIBRARY_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
-        RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
+target_optimize(vtflib_static)
 
 # I'm not distributing the benchmark program, so this should be fine
 target_link_libraries(${TARGET}_bench PUBLIC vtflib_static)
