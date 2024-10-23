1d917b18ce6dc323a15376fbc181f19931e2a372
craftablescience <me@craftablescience.info>
2024-10-23 03:16:27 -0400

    feat(python): add author property

--- lang/python/dist/sourcepp/__init__.py
+++ lang/python/dist/sourcepp/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from ._sourcepp_python import __doc__, __version__, gamepp, sourcepp, steampp, vcryptpp
+from ._sourcepp_python import __author__, __doc__, __version__, gamepp, sourcepp, steampp, vcryptpp
 
-__all__ = ['__doc__', '__version__', 'gamepp', 'sourcepp', 'steampp', 'vcryptpp']
+__all__ = ['__author__', '__doc__', '__version__', 'gamepp', 'sourcepp', 'steampp', 'vcryptpp']
