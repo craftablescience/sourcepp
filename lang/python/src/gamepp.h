edda7982905f1ddc5c5229cb20fabc9b0868f4b9
craftablescience <me@craftablescience.info>
2024-10-26 19:17:39 -0400

    chore(python): reformat files a bit

--- lang/python/src/gamepp.h
+++ lang/python/src/gamepp.h
@@ -14,16 +14,16 @@ inline void register_python(py::module_& m) {
 	using namespace gamepp;
 
 	py::class_<GameInstance>(gamepp, "GameInstance")
-			.def_static("find", &GameInstance::find, py::arg("window_name_override") = "")
+			.def_static("find",                    &GameInstance::find,       py::arg("window_name_override") = "")
 			.def_property_readonly("window_title", &GameInstance::getWindowTitle)
-			.def_property_readonly("window_pos", &GameInstance::getWindowPos)
-			.def_property_readonly("window_size", &GameInstance::getWindowSize)
-			.def("command", &GameInstance::command)
-			.def("input_begin", &GameInstance::inputBegin)
-			.def("input_end", &GameInstance::inputEnd)
-			.def("input_once", &GameInstance::inputOnce)
-			.def("input_hold", &GameInstance::inputHold)
-			.def("wait", &GameInstance::wait);
+			.def_property_readonly("window_pos",   &GameInstance::getWindowPos)
+			.def_property_readonly("window_size",  &GameInstance::getWindowSize)
+			.def("command",                        &GameInstance::command,    py::arg("command"))
+			.def("input_begin",                    &GameInstance::inputBegin, py::arg("input"))
+			.def("input_end",                      &GameInstance::inputEnd,   py::arg("input"))
+			.def("input_once",                     &GameInstance::inputOnce,  py::arg("input"))
+			.def("input_hold",                     &GameInstance::inputHold,  py::arg("input"), py::arg("sec"))
+			.def("wait",                           &GameInstance::wait,       py::arg("sec"));
 }
 
 } // namespace gamepp
--- lang/python/src/vcryptpp.h
+++ lang/python/src/vcryptpp.h
@@ -36,34 +36,34 @@ inline void register_python(py::module_& m) {
 			auto KnownCodes = VICE.def_submodule("KnownCodes");
 			using namespace KnownCodes;
 
-			KnownCodes.attr("DEFAULT") = DEFAULT;
-			KnownCodes.attr("CONTAGION_WEAPONS") = CONTAGION_WEAPONS;
-			KnownCodes.attr("CONTAGION_SCRIPTS") = CONTAGION_SCRIPTS;
-			KnownCodes.attr("COUNTER_STRIKE_SOURCE") = COUNTER_STRIKE_SOURCE;
+			KnownCodes.attr("DEFAULT")                         = DEFAULT;
+			KnownCodes.attr("CONTAGION_WEAPONS")               = CONTAGION_WEAPONS;
+			KnownCodes.attr("CONTAGION_SCRIPTS")               = CONTAGION_SCRIPTS;
+			KnownCodes.attr("COUNTER_STRIKE_SOURCE")           = COUNTER_STRIKE_SOURCE;
 			KnownCodes.attr("COUNTER_STRIKE_GLOBAL_OFFENSIVE") = COUNTER_STRIKE_GLOBAL_OFFENSIVE;
-			KnownCodes.attr("COUNTER_STRIKE_2") = COUNTER_STRIKE_2;
-			KnownCodes.attr("COUNTER_STRIKE_PROMOD") = COUNTER_STRIKE_PROMOD;
-			KnownCodes.attr("DAY_OF_DEFEAT_SOURCE") = DAY_OF_DEFEAT_SOURCE;
-			KnownCodes.attr("DYSTOPIA_1_2") = DYSTOPIA_1_2;
-			KnownCodes.attr("DYSTOPIA_1_3") = DYSTOPIA_1_3;
-			KnownCodes.attr("GOLDEN_EYE_SOURCE") = GOLDEN_EYE_SOURCE;
-			KnownCodes.attr("HALF_LIFE_2_CTF") = HALF_LIFE_2_CTF;
-			KnownCodes.attr("HALF_LIFE_2_DM") = HALF_LIFE_2_DM;
-			KnownCodes.attr("INSURGENCY") = INSURGENCY;
-			KnownCodes.attr("LEFT_4_DEAD_2") = LEFT_4_DEAD_2;
-			KnownCodes.attr("NO_MORE_ROOM_IN_HELL") = NO_MORE_ROOM_IN_HELL;
-			KnownCodes.attr("NUCLEAR_DAWN") = NUCLEAR_DAWN;
-			KnownCodes.attr("TACTICAL_INTERVENTION") = TACTICAL_INTERVENTION;
-			KnownCodes.attr("TEAM_FORTRESS_2") = TEAM_FORTRESS_2;
-			KnownCodes.attr("TEAM_FORTRESS_2_ITEMS") = TEAM_FORTRESS_2_ITEMS;
-			KnownCodes.attr("THE_SHIP") = THE_SHIP;
-			KnownCodes.attr("ZOMBIE_PANIC_SOURCE") = ZOMBIE_PANIC_SOURCE;
+			KnownCodes.attr("COUNTER_STRIKE_2")                = COUNTER_STRIKE_2;
+			KnownCodes.attr("COUNTER_STRIKE_PROMOD")           = COUNTER_STRIKE_PROMOD;
+			KnownCodes.attr("DAY_OF_DEFEAT_SOURCE")            = DAY_OF_DEFEAT_SOURCE;
+			KnownCodes.attr("DYSTOPIA_1_2")                    = DYSTOPIA_1_2;
+			KnownCodes.attr("DYSTOPIA_1_3")                    = DYSTOPIA_1_3;
+			KnownCodes.attr("GOLDEN_EYE_SOURCE")               = GOLDEN_EYE_SOURCE;
+			KnownCodes.attr("HALF_LIFE_2_CTF")                 = HALF_LIFE_2_CTF;
+			KnownCodes.attr("HALF_LIFE_2_DM")                  = HALF_LIFE_2_DM;
+			KnownCodes.attr("INSURGENCY")                      = INSURGENCY;
+			KnownCodes.attr("LEFT_4_DEAD_2")                   = LEFT_4_DEAD_2;
+			KnownCodes.attr("NO_MORE_ROOM_IN_HELL")            = NO_MORE_ROOM_IN_HELL;
+			KnownCodes.attr("NUCLEAR_DAWN")                    = NUCLEAR_DAWN;
+			KnownCodes.attr("TACTICAL_INTERVENTION")           = TACTICAL_INTERVENTION;
+			KnownCodes.attr("TEAM_FORTRESS_2")                 = TEAM_FORTRESS_2;
+			KnownCodes.attr("TEAM_FORTRESS_2_ITEMS")           = TEAM_FORTRESS_2_ITEMS;
+			KnownCodes.attr("THE_SHIP")                        = THE_SHIP;
+			KnownCodes.attr("ZOMBIE_PANIC_SOURCE")             = ZOMBIE_PANIC_SOURCE;
 
-			KnownCodes.attr("EKV_GPU_DEFAULT") = EKV_GPU_DEFAULT;
-			KnownCodes.attr("EKV_GPU_ALIEN_SWARM") = EKV_GPU_ALIEN_SWARM;
+			KnownCodes.attr("EKV_GPU_DEFAULT")       = EKV_GPU_DEFAULT;
+			KnownCodes.attr("EKV_GPU_ALIEN_SWARM")   = EKV_GPU_ALIEN_SWARM;
 			KnownCodes.attr("EKV_GPU_LEFT_4_DEAD_1") = EKV_GPU_LEFT_4_DEAD_1;
 			KnownCodes.attr("EKV_GPU_LEFT_4_DEAD_2") = EKV_GPU_LEFT_4_DEAD_2;
-			KnownCodes.attr("EKV_GPU_PORTAL_2") = EKV_GPU_PORTAL_2;
+			KnownCodes.attr("EKV_GPU_PORTAL_2")      = EKV_GPU_PORTAL_2;
 		}
 
 		VICE.def("decrypt_bytes", [](const py::bytes& data, std::string_view code = KnownCodes::DEFAULT) -> py::bytes {
