a42c81290b4c542098d7eaa52efe711193f168d9
craftablescience <me@craftablescience.info>
2024-10-26 19:17:39 -0400

    chore(python): reformat files a bit

--- lang/python/src/steampp.h
+++ lang/python/src/steampp.h
@@ -14,21 +14,21 @@ inline void register_python(py::module_& m) {
 	using namespace steampp;
 
 	py::class_<Steam>(steampp, "Steam")
-	        .def(py::init<>())
-			.def_property_readonly("install_dir", &Steam::getInstallDir)
-			.def_property_readonly("library_dirs", &Steam::getLibraryDirs)
-			.def_property_readonly("sourcemod_dir", &Steam::getSourceModDir)
+			.def(py::init<>())
+			.def_property_readonly("install_dir",    &Steam::getInstallDir)
+			.def_property_readonly("library_dirs",   &Steam::getLibraryDirs)
+			.def_property_readonly("sourcemod_dir",  &Steam::getSourceModDir)
 			.def_property_readonly("installed_apps", &Steam::getInstalledApps)
-			.def("is_app_installed", &Steam::isAppInstalled)
-			.def("get_app_name", &Steam::getAppName)
-			.def("get_app_install_dir", &Steam::getAppInstallDir)
-			.def("get_app_icon_path", &Steam::getAppIconPath)
-			.def("get_app_logo_path", &Steam::getAppLogoPath)
-			.def("get_app_box_art_path", &Steam::getAppBoxArtPath)
-			.def("get_app_store_art_path", &Steam::getAppStoreArtPath)
-			.def("is_app_using_source_engine", &Steam::isAppUsingSourceEngine)
-			.def("is_app_using_source_2_engine", &Steam::isAppUsingSource2Engine)
-			.def_property_readonly("valid", &Steam::operator bool);
+			.def("is_app_installed",                 &Steam::isAppInstalled,          py::arg("appID"))
+			.def("get_app_name",                     &Steam::getAppName,              py::arg("appID"))
+			.def("get_app_install_dir",              &Steam::getAppInstallDir,        py::arg("appID"))
+			.def("get_app_icon_path",                &Steam::getAppIconPath,          py::arg("appID"))
+			.def("get_app_logo_path",                &Steam::getAppLogoPath,          py::arg("appID"))
+			.def("get_app_box_art_path",             &Steam::getAppBoxArtPath,        py::arg("appID"))
+			.def("get_app_store_art_path",           &Steam::getAppStoreArtPath,      py::arg("appID"))
+			.def("is_app_using_source_engine",       &Steam::isAppUsingSourceEngine,  py::arg("appID"))
+			.def("is_app_using_source_2_engine",     &Steam::isAppUsingSource2Engine, py::arg("appID"))
+			.def_property_readonly("__bool__",       &Steam::operator bool,           py::is_operator());
 }
 
 } // namespace steampp
