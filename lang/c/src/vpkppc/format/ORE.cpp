379263e701db164e90e3eb6bf0829ee9a36773a8
craftablescience <me@craftablescience.info>
2024-12-17 01:17:54 -0500

    vpkpp: add read-only support for ORE (Narbacular Drop) pack files

---
+++ lang/c/src/vpkppc/format/ORE.cpp
@@ -0,0 +1,29 @@
+#include <vpkppc/format/PAK.h>
+
+#include <vpkpp/format/ORE.h>
+
+#include <sourceppc/Helpers.h>
+
+using namespace vpkpp;
+
+SOURCEPP_API vpkpp_pack_file_handle_t vpkpp_ore_create(const char* path) {
+	SOURCEPP_EARLY_RETURN_VAL(path, nullptr);
+
+	auto packFile = ORE::create(path);
+	if (!packFile) {
+		return nullptr;
+	}
+	return packFile.release();
+}
+
+SOURCEPP_API vpkpp_pack_file_handle_t vpkpp_ore_open(const char* path, vpkpp_entry_callback_t callback) {
+	SOURCEPP_EARLY_RETURN_VAL(path, nullptr);
+
+	auto packFile = ORE::open(path, callback ? [callback](const std::string& path, const Entry& entry) {
+		callback(path.c_str(), const_cast<Entry*>(&entry));
+	} : static_cast<PackFile::EntryCallback>(nullptr));
+	if (!packFile) {
+		return nullptr;
+	}
+	return packFile.release();
+}
