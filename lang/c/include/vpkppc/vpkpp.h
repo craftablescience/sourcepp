9ca94ed9d9495b24d983302169a32f9967ab54be
craftablescience <me@craftablescience.info>
2025-01-13 12:02:40 -0500

    c: update vpkpp header to include all C headers in the library, matches every other header named after the library

--- lang/c/include/vpkppc/vpkpp.h
+++ lang/c/include/vpkppc/vpkpp.h
@@ -5,4 +5,19 @@
  * include it the same way as any of the other SourcePP libraries.
  */
 
+#include "format/FPX.h"
+#include "format/GCF.h"
+#include "format/GMA.h"
+#include "format/OO7.h"
+#include "format/OL.h"
+#include "format/ORE.h"
+#include "format/PAK.h"
+#include "format/PCK.h"
+#include "format/VPK.h"
+#include "format/VPK_VTMB.h"
+#include "format/WAD3.h"
+#include "format/ZIP.h"
+#include "Attribute.h"
+#include "Entry.h"
+#include "Options.h"
 #include "PackFile.h"
