01e27d0a6bcf62fb53efb02f710cc74f10826a9f
craftablescience <me@craftablescience.info>
2025-01-26 18:13:07 -0500

    sourcepp: fix valve lzma compression function

--- src/sourcepp/compression/LZMA.cpp
+++ src/sourcepp/compression/LZMA.cpp
@@ -6,54 +6,45 @@
 using namespace sourcepp;
 
 std::optional<std::vector<std::byte>> compression::compressValveLZMA(std::span<const std::byte> data, uint8_t compressLevel) {
-	// Preallocate extra 4 bytes for Valve LZMA header signature
-	std::vector<std::byte> compressedData(sizeof(uint32_t));
-	std::array<std::byte, 8192> compressedChunk{};
-
-	lzma_stream stream{
-		.next_in = reinterpret_cast<const uint8_t*>(data.data()),
-		.avail_in = data.size(),
-		.next_out = reinterpret_cast<uint8_t*>(compressedChunk.data()),
-		.avail_out = compressedChunk.size(),
-	};
+	// Shift over 4 bytes for Valve LZMA header signature
+	std::vector<std::byte> compressedData(sizeof(uint32_t) + data.size() * 2);
 
 	lzma_options_lzma options{};
-	lzma_lzma_preset(&options, std::clamp<uint8_t>(compressLevel, 0, 9));
+	if (lzma_lzma_preset(&options, std::clamp<uint8_t>(compressLevel, 0, 9))) {
+		return std::nullopt;
+	}
+
+	lzma_stream stream = LZMA_STREAM_INIT;
 
 	if (lzma_alone_encoder(&stream, &options) != LZMA_OK) {
 		lzma_end(&stream);
 		return std::nullopt;
 	}
 
-	lzma_ret ret;
-	do {
-		stream.next_out = reinterpret_cast<uint8_t*>(compressedChunk.data());
-		stream.avail_out = compressedChunk.size();
+	stream.next_in = reinterpret_cast<const uint8_t*>(data.data());
+	stream.avail_in = data.size();
+	stream.next_out = reinterpret_cast<uint8_t*>(compressedData.data() + sizeof(uint32_t));
+	stream.avail_out = compressedData.size() - sizeof(uint32_t);
 
-		ret = lzma_code(&stream, LZMA_RUN);
-		compressedData.insert(compressedData.end(), compressedChunk.begin(), compressedChunk.begin() + compressedChunk.size() - static_cast<std::ptrdiff_t>(stream.avail_out));
-	} while (ret == LZMA_OK);
-
-	ret = lzma_code(&stream, LZMA_FINISH);
-	if (ret != LZMA_OK && ret != LZMA_STREAM_END) {
-		lzma_end(&stream);
+	lzma_ret ret = lzma_code(&stream, LZMA_FINISH);
+	lzma_end(&stream);
+	if (ret != LZMA_STREAM_END) {
 		return std::nullopt;
 	}
-	lzma_end(&stream);
 
-	{
-		// Switch out normal header with Valve one
-		BufferStream compressedStream{compressedData};
-		compressedStream << VALVE_LZMA_SIGNATURE;
-		const auto properties = compressedStream.read<uint8_t>();
-		const auto dictionarySize = compressedStream.read<uint32_t>();
-		compressedStream
-			.seek_u(sizeof(uint32_t))
-			.write<uint32_t>(data.size())
-			.write(compressedData.size() - (sizeof(uint32_t) * 3) + (sizeof(uint8_t) * 5))
-			.write(properties)
-			.write(dictionarySize);
-	}
+	// Switch out normal header with Valve one
+	BufferStream compressedStream{compressedData};
+	compressedStream.seek(0).write(VALVE_LZMA_SIGNATURE);
+	const auto properties = compressedStream.read<uint8_t>();
+	const auto dictionarySize = compressedStream.read<uint32_t>();
+	compressedStream
+		.seek_u(sizeof(uint32_t))
+		.write<uint32_t>(data.size())
+		.write(compressedData.size() - (sizeof(uint32_t) * 3) + (sizeof(uint8_t) * 5))
+		.write(properties)
+		.write(dictionarySize);
+
+	compressedData.resize(stream.total_out + sizeof(uint32_t));
 	return compressedData;
 }
 
@@ -84,12 +75,11 @@ std::optional<std::vector<std::byte>> compression::decompressValveLZMA(std::span
 		uncompressedData.resize(uncompressedLength);
 	}
 
-	lzma_stream stream{
-		.next_in = reinterpret_cast<const uint8_t*>(compressedData.data()),
-		.avail_in = compressedData.size(),
-		.next_out = reinterpret_cast<uint8_t*>(uncompressedData.data()),
-		.avail_out = uncompressedData.size(),
-	};
+	lzma_stream stream = LZMA_STREAM_INIT;
+	stream.next_in = reinterpret_cast<const uint8_t*>(compressedData.data());
+	stream.avail_in = compressedData.size();
+	stream.next_out = reinterpret_cast<uint8_t*>(uncompressedData.data());
+	stream.avail_out = uncompressedData.size();
 
 	if (lzma_alone_decoder(&stream, UINT64_MAX) != LZMA_OK) {
 		lzma_end(&stream);
