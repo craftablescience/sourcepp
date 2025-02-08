44034f0d4985c55082dcbe4ba7d8597ae3e2415b
craftablescience <me@craftablescience.info>
2025-02-08 16:21:40 -0500

    c: change a typedef and some enum constants to be properly namespaced

--- lang/c/include/sourceppc/Helpers.h
+++ lang/c/include/sourceppc/Helpers.h
@@ -2,14 +2,14 @@
 
 #define SOURCEPP_EARLY_RETURN(var) \
 	do {                           \
-		if (!var) {                \
+		if (!(var)) {              \
 			return;                \
 		}                          \
 	} while (0)
 
 #define SOURCEPP_EARLY_RETURN_VAL(var, value) \
 	do {                                      \
-		if (!var) {                           \
+		if (!(var)) {                         \
 			return value;                     \
 		}                                     \
 	} while (0)
