a65e108475faf27455a3fd971d27973ae3c52ffe
craftablescience <me@craftablescience.info>
2025-02-23 16:48:39 -0500

    c: update vpkpp bindings

---
+++ lang/c/src/vpkppc/format/VPP.cpp
@@ -0,0 +1,27 @@
+#include <vpkppc/format/VPP.h>
+
+#include <vpkpp/format/VPP.h>
+
+#include <sourceppc/Convert.hpp>
+#include <sourceppc/Helpers.h>
+
+using namespace vpkpp;
+
+SOURCEPP_API vpkpp_pack_file_handle_t vpkpp_vpp_open(const char* path, vpkpp_entry_callback_t callback) {
+	SOURCEPP_EARLY_RETURN_VAL(path, nullptr);
+
+	auto packFile = VPP::open(path, callback ? [callback](const std::string& path, const Entry& entry) {
+		callback(path.c_str(), const_cast<Entry*>(&entry));
+	} : static_cast<PackFile::EntryCallback>(nullptr));
+	if (!packFile) {
+		return nullptr;
+	}
+	return packFile.release();
+}
+
+// REQUIRES MANUAL FREE: sourcepp_string_free
+SOURCEPP_API sourcepp_string_t vpkpp_vpp_guid(vpkpp_pack_file_handle_t handle) {
+	SOURCEPP_EARLY_RETURN_VAL(handle, SOURCEPP_STRING_INVALID);
+
+	return Convert::toString(VPP::GUID);
+}
