bdefeb24d64410a6ea012b58e11a3c51cd0e39d4
craftablescience <me@craftablescience.info>
2025-07-06 15:47:40 -0700

    c: add macros for global values

--- lang/c/include/sourceppc/API.h
+++ lang/c/include/sourceppc/API.h
@@ -34,21 +34,24 @@
 #define SOURCEPP_API SOURCEPP_EXTERN SOURCEPP_EXPORT
 
 // For CWrap language bindings
-#define SOURCEPP_FUNCTN(ns, rt, fn, ...)     SOURCEPP_API rt ns##_##fn(__VA_ARGS__)
-#define SOURCEPP_STATIC(ns, cs, rt, mt, ...) SOURCEPP_API rt ns##_##cs##_##mt(__VA_ARGS__)
-#define SOURCEPP_METHOD(ns, cs, rt, mt)      SOURCEPP_API rt ns##_##cs##_##mt(ns##_##cs##_handle_t handle)
-#define SOURCEPP_METHOE(ns, cs, rt, mt, ...) SOURCEPP_API rt ns##_##cs##_##mt(ns##_##cs##_handle_t handle, __VA_ARGS__)
+#define SOURCEPP_FUNCTION(ns, rt, fn, ...)      SOURCEPP_API rt ns##_##fn(__VA_ARGS__)
+#define SOURCEPP_STATIC(ns, cs, rt, mt, ...)    SOURCEPP_API rt ns##_##cs##_##mt(__VA_ARGS__)
+#define SOURCEPP_METHOD(ns, cs, rt, mt)         SOURCEPP_API rt ns##_##cs##_##mt(ns##_##cs##_handle_t handle)
+#define SOURCEPP_METHOE(ns, cs, rt, mt, ...)    SOURCEPP_API rt ns##_##cs##_##mt(ns##_##cs##_handle_t handle, __VA_ARGS__)
 
-#define SOURCEPP_HANDLE(ns, cs)              typedef void* ns##_##cs##_handle_t
-#define SOURCEPP_TYPEDF(ns, cs, tp)          typedef tp ns##_##cs##_t
+#define SOURCEPP_HANDLE(ns, cs)                 typedef void* ns##_##cs##_handle_t
+#define SOURCEPP_TYPEDF(ns, cs, tp)             typedef tp ns##_##cs##_t
 
-#define SOURCEPP_STRUCT_BEGIN()              SOURCEPP_EXTERN typedef struct
-#define SOURCEPP_STRUCT_FIELD(tp, nm, df)    tp nm
-#define SOURCEPP_STRUCT_END(ns, st)          ns##_##st##_t
+#define SOURCEPP_STRUCT_BEGIN()                 SOURCEPP_EXTERN typedef struct
+#define SOURCEPP_STRUCT_FIELD(tp, nm, df)       tp nm
+#define SOURCEPP_STRUCT_END(ns, st)             ns##_##st##_t
 
-#define SOURCEPP_ENUM_BEGIN()                SOURCEPP_EXTERN typedef enum
-#define SOURCEPP_ENUM_VALUD(ns, en, nm)      ns##_##en##_##nm
-#define SOURCEPP_ENUM_VALUE(ns, en, nm, vl)  ns##_##en##_##nm = vl
-#define SOURCEPP_ENUM_END(ns, en)            ns##_##en##_e
+#define SOURCEPP_ENUM_BEGIN()                   SOURCEPP_EXTERN typedef enum
+#define SOURCEPP_ENUM_VALUD(ns, en, nm)         ns##_##en##_##nm
+#define SOURCEPP_ENUM_VALUE(ns, en, nm, vl)     ns##_##en##_##nm = vl
+#define SOURCEPP_ENUM_END(ns, en)               ns##_##en##_e
 
-// todo: global values
+#define SOURCEPP_GLOBAL_EXT(ns, tp, nm, vl)     extern tp ns##_##nm;
+#define SOURCEPP_GLOBAL_INT(ns, tp, nm, vl)     tp ns##_##nm = vl;
+#define SOURCEPP_CLSVAR_EXT(ns, cs, tp, nm, vl) extern tp ns##_##cs##_##nm;
+#define SOURCEPP_CLSVAR_INT(ns, cs, tp, nm, vl) tp ns##_##cs##_##nm = vl;
--- lang/c/include/vcryptppc/VICE.h
+++ lang/c/include/vcryptppc/VICE.h
@@ -2,35 +2,35 @@
 
 #include <sourceppc/Buffer.h>
 
-#define VCRYPTPP_VICE_KNOWN_CODES_DEFAULT                         "x9Ke0BY7";
-#define VCRYPTPP_VICE_BLOODY_GOOD_TIME                            "K4PeJwL7"
-#define VCRYPTPP_VICE_KNOWN_CODES_CONTAGION_WEAPONS               "fUk0fF69"
-#define VCRYPTPP_VICE_KNOWN_CODES_CONTAGION_SCRIPTS               "5!rrFz6p"
-#define VCRYPTPP_VICE_KNOWN_CODES_COUNTER_STRIKE_SOURCE           "d7NSuLq2"
-#define VCRYPTPP_VICE_KNOWN_CODES_COUNTER_STRIKE_GLOBAL_OFFENSIVE VCRYPTPP_VICE_KNOWN_CODES_COUNTER_STRIKE_SOURCE
-#define VCRYPTPP_VICE_KNOWN_CODES_COUNTER_STRIKE_2                VCRYPTPP_VICE_KNOWN_CODES_COUNTER_STRIKE_GLOBAL_OFFENSIVE
-#define VCRYPTPP_VICE_KNOWN_CODES_COUNTER_STRIKE_PROMOD           "H1aRQ0n1"
-#define VCRYPTPP_VICE_KNOWN_CODES_DAY_OF_DEFEAT_SOURCE            "Wl0u5B3F"
-#define VCRYPTPP_VICE_KNOWN_CODES_DYSTOPIA_1_2                    "pH3apO8w"
-#define VCRYPTPP_VICE_KNOWN_CODES_DYSTOPIA_1_3                    "G8stUh3F"
-#define VCRYPTPP_VICE_KNOWN_CODES_GOLDEN_EYE_SOURCE               "Gr3naDes"
-#define VCRYPTPP_VICE_KNOWN_CODES_HALF_LIFE_2_CTF                 "R1dj3axP"
-#define VCRYPTPP_VICE_KNOWN_CODES_HALF_LIFE_2_DM                  VCRYPTPP_VICE_KNOWN_CODES_DEFAULT
-#define VCRYPTPP_VICE_KNOWN_CODES_INSURGENCY                      "DrA5e3EB"
-#define VCRYPTPP_VICE_KNOWN_CODES_LEFT_4_DEAD_2                   "SDhfi878"
-#define VCRYPTPP_VICE_KNOWN_CODES_NO_MORE_ROOM_IN_HELL            "lREeeapA"
-#define VCRYPTPP_VICE_KNOWN_CODES_NUCLEAR_DAWN                    "TA+*veh9"
-#define VCRYPTPP_VICE_KNOWN_CODES_TACTICAL_INTERVENTION           "71B4Dt1Z"
-#define VCRYPTPP_VICE_KNOWN_CODES_TEAM_FORTRESS_2                 "E2NcUkG2"
-#define VCRYPTPP_VICE_KNOWN_CODES_TEAM_FORTRESS_2_ITEMS           "A5fSXbf7"
-#define VCRYPTPP_VICE_KNOWN_CODES_THE_SHIP                        "eb3A4m79"
-#define VCRYPTPP_VICE_KNOWN_CODES_ZOMBIE_PANIC_SOURCE             "5R0ni0pZ"
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_default,                         "x9Ke0BY7");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_bloody_good_time,                "K4PeJwL7");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_contagion_weapons,               "fUk0fF69");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_contagion_scripts,               "5!rrFz6p");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_counter_strike_source,           "d7NSuLq2");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_counter_strike_global_offensive, "d7NSuLq2");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_counter_strike_2,                "d7NSuLq2");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_counter_strike_promod,           "H1aRQ0n1");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_day_of_defeat_source,            "Wl0u5B3F");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_dystopia_1_2,                    "pH3apO8w");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_dystopia_1_3,                    "G8stUh3F");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_golden_eye_source,               "Gr3naDes");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_half_life_2_ctf,                 "R1dj3axP");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_half_life_2_dm,                  "x9Ke0BY7");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_insurgency,                      "DrA5e3EB");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_left_4_dead_2,                   "SDhfi878");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_no_more_room_in_hell,            "lREeeapA");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_nuclear_dawn,                    "TA+*veh9");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_tactical_intervention,           "71B4Dt1Z");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_team_fortress_2,                 "E2NcUkG2");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_team_fortress_2_items,           "A5fSXbf7");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_the_ship,                        "eb3A4m79");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_zombie_panic_source,             "5R0ni0pZ");
 
-#define VCRYPTPP_VICE_KNOWN_CODES_EKV_GPU_DEFAULT                 "X8bU2qll"
-#define VCRYPTPP_VICE_KNOWN_CODES_EKV_GPU_ALIEN_SWARM             "sW9.JupP"
-#define VCRYPTPP_VICE_KNOWN_CODES_EKV_GPU_LEFT_4_DEAD_1           "zp14Hi(]"
-#define VCRYPTPP_VICE_KNOWN_CODES_EKV_GPU_LEFT_4_DEAD_2           VCRYPTPP_VICE_KNOWN_CODES_EKV_GPU_LEFT_4_DEAD_1
-#define VCRYPTPP_VICE_KNOWN_CODES_EKV_GPU_PORTAL_2                "UrE66!Ap"
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_ekv_gpu_default,                 "X8bU2qll");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_ekv_gpu_alien_swarm,             "sW9.JupP");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_ekv_gpu_left_4_dead_1,           "zp14Hi(]");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_ekv_gpu_left_4_dead_2,           "zp14Hi(]");
+SOURCEPP_CLSVAR_EXT(vcryptpp, vice, const char*, known_codes_ekv_gpu_portal_2,                "UrE66!Ap");
 
 SOURCEPP_STATIC(vcryptpp, vice, sourcepp_buffer_t, encrypt, const unsigned char* buffer, size_t bufferLen, const char* code); // REQUIRES MANUAL FREE: sourcepp_buffer_free
 SOURCEPP_STATIC(vcryptpp, vice, sourcepp_buffer_t, decrypt, const unsigned char* buffer, size_t bufferLen, const char* code); // REQUIRES MANUAL FREE: sourcepp_buffer_free
