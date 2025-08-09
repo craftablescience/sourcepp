3377d18c528b0c7cb920e699ca0fd04735aff80d
craftablescience <me@craftablescience.info>
2025-08-08 22:40:31 -0700

    cmake: cwrap target might not exist if only C bindings are enabled

--- lang/c/src/sourceppc/_sourceppc.cmake
+++ lang/c/src/sourceppc/_sourceppc.cmake
@@ -8,4 +8,6 @@ list(APPEND ${PROJECT_NAME}c_SOURCES
         "${CMAKE_CURRENT_LIST_DIR}/Convert.cpp"
         "${CMAKE_CURRENT_LIST_DIR}/String.cpp")
 
-target_compile_definitions(${PROJECT_NAME}_cwrap PRIVATE -DSOURCEPP_PATH="${CMAKE_CURRENT_SOURCE_DIR}/lang/c/include/sourceppc")
+if(TARGET ${PROJECT_NAME}_cwrap)
+    target_compile_definitions(${PROJECT_NAME}_cwrap PRIVATE -DSOURCEPP_PATH="${CMAKE_CURRENT_SOURCE_DIR}/lang/c/include/sourceppc")
+endif()
