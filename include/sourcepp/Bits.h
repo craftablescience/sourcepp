7ab8776c32b06b6e684788467c6a373a4c9f8405
yuuko <yuuko+of@partyvan.io>
2025-09-11 18:10:08 -0700

    sourcepp: more LERep constructors + convenient read

--- include/sourcepp/Bits.h
+++ include/sourcepp/Bits.h
@@ -13,6 +13,8 @@ namespace sourcepp::bits {
 /// in structures destined for I/O buffers is suspect, and, in either case, should likely
 /// be replaced by some invocation of this class. This can be syntactically cumbersome,
 /// so convenient aliases are provided with f32le, etc.
+///
+/// Special provisions for arithmetic outside possible implicit conversions are intentionally omitted.
 template<sourcepp::math::Arithmetic A>
 class LERep : public std::array<std::byte, sizeof(A)> {
 	using uint_according = typename std::conditional<sizeof(A) == 8,
@@ -59,7 +61,28 @@ public:
 	/// (`LERep<A>` ⇒ `A`) ∧ (`A` ⇒ `B`) ⇒ (`LERep<A>` ⇒ `B`).
 	template<sourcepp::math::Arithmetic B> requires std::convertible_to<A, B>
 	constexpr operator B() const { return static_cast<B>(this->operator A()); }
+
+	/// Permits uninitialized LERep, for parity with the associated A.
+	constexpr LERep() {}
+
+	/// Construct a value from a byte buffer.
+	constexpr explicit LERep(std::span<const std::byte, sizeof(A)> arr) {
+		std::memcpy(this, arr.data(), sizeof(A));
+	}
 };
+
+/// Read a given value of type A from a pointer into arbitrary data presumed to hold a little-endian representation of A.
+/// Subject to the usual litany of cautions about
+///
+///     *reinterpret_cast<T *>(p)
+///
+/// , such invocations, where spuriously dependent upon host byte order, may be replaced with:
+///
+///     reinterpret_le<T>(p)
+template<sourcepp::math::Arithmetic A>
+[[nodiscard]] constexpr A reinterpret_le(const void *p) {
+	return LERep<A>(std::span<const std::byte, sizeof(A)>(static_cast<const std::byte *>(p), sizeof(A))).operator A();
+}
 
 #define SOURCEPP_LEREP_DEFINE(N, V)\
 	using N##le = LERep<V>
