7a79d22bf75998862ef744bb28bb25891dfc04f0
craftablescience <me@craftablescience.info>
2025-09-28 02:38:51 -0700

    cmake: use add_sourcepp_remote_library in more places

--- cmake/AddRemoteLibrary.cmake
+++ cmake/AddRemoteLibrary.cmake
@@ -1,9 +1,14 @@
 include_guard(GLOBAL)
 
+include(FetchContent)
+
 function(add_sourcepp_remote_library NAME REPOSITORY TAG)
-    string(LENGTH "${TAG}" TAG_SIZE)
-    if(NOT TAG_SIZE EQUAL 40)
-        message(FATAL_ERROR "For the sake of making SourcePP builds reproducible, dependencies must be pinned to specific commits. ${NAME} is not.")
+    cmake_parse_arguments(PARSE_ARGV 3 OPTIONS "ALLOW_NOT_PINNED" "" "")
+    if(NOT OPTIONS_ALLOW_NOT_PINNED)
+        string(LENGTH "${TAG}" TAG_SIZE)
+        if(NOT TAG_SIZE EQUAL 40)
+            message(FATAL_ERROR "For the sake of making SourcePP builds reproducible, dependencies must be pinned to specific commits. ${NAME} is not.")
+        endif()
     endif()
     if(NOT TARGET NAME)
         message(STATUS "Fetching ${NAME} ${REPOSITORY} ${TAG}")
