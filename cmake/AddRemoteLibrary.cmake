2deef0a269d0730d8cc977360f1259ff2e1327ba
craftablescience <me@craftablescience.info>
2025-09-28 07:59:34 -0700

    cmake: add EXCLUDE_FROM_ALL back to webm to fix python build

--- cmake/AddRemoteLibrary.cmake
+++ cmake/AddRemoteLibrary.cmake
@@ -3,7 +3,7 @@ include_guard(GLOBAL)
 include(FetchContent)
 
 function(add_sourcepp_remote_library NAME REPOSITORY TAG)
-    cmake_parse_arguments(PARSE_ARGV 3 OPTIONS "ALLOW_NOT_PINNED" "" "")
+    cmake_parse_arguments(PARSE_ARGV 3 OPTIONS "ALLOW_NOT_PINNED;EXCLUDE_FROM_ALL" "" "")
     if(NOT OPTIONS_ALLOW_NOT_PINNED)
         string(LENGTH "${TAG}" TAG_SIZE)
         if(NOT TAG_SIZE EQUAL 40)
@@ -13,9 +13,16 @@ function(add_sourcepp_remote_library NAME REPOSITORY TAG)
     if(NOT TARGET NAME)
         message(STATUS "Fetching ${NAME} ${REPOSITORY} ${TAG}")
         string(REPLACE ":" "_" NAME_CLEAN "${NAME}")
-        FetchContent_Declare(${NAME_CLEAN}
-                GIT_REPOSITORY "${REPOSITORY}.git"
-                GIT_TAG        "${TAG}")
+        if(OPTIONS_EXCLUDE_FROM_ALL)
+            FetchContent_Declare(${NAME_CLEAN}
+                    GIT_REPOSITORY "${REPOSITORY}.git"
+                    GIT_TAG        "${TAG}"
+                    EXCLUDE_FROM_ALL)
+        else()
+            FetchContent_Declare(${NAME_CLEAN}
+                    GIT_REPOSITORY "${REPOSITORY}.git"
+                    GIT_TAG        "${TAG}")
+        endif()
         FetchContent_MakeAvailable(${NAME_CLEAN})
     endif()
     return(PROPAGATE "${NAME_CLEAN}_POPULATED" "${NAME_CLEAN}_SOURCE_DIR" "${NAME_CLEAN}_BINARY_DIR")
