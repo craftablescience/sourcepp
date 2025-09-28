10dfab0307db418ed5e319bd60fdf2c02e5a05cf
craftablescience <me@craftablescience.info>
2025-09-28 01:16:44 -0700

    c: set DLL windows icon to sourcepp logo

--- cmake/AddPrettyParser.cmake
+++ cmake/AddPrettyParser.cmake
@@ -5,7 +5,7 @@ function(add_pretty_parser TARGET)
     cmake_parse_arguments(PARSE_ARGV 1 OPTIONS "C" "" "DEPS;DEPS_PUBLIC;DEPS_INTERFACE;PRECOMPILED_HEADERS;SOURCES")
 
     if(OPTIONS_C)
-        add_library(sourcepp_${TARGET}c SHARED ${${PROJECT_NAME}c_SOURCES} ${OPTIONS_PRECOMPILED_HEADERS} ${OPTIONS_SOURCES})
+        add_library(sourcepp_${TARGET}c SHARED ${${PROJECT_NAME}c_SOURCES} ${OPTIONS_PRECOMPILED_HEADERS} ${OPTIONS_SOURCES} "$<$<BOOL:${WIN32}>:${CMAKE_CURRENT_SOURCE_DIR}/branding/logo.rc>")
         add_library(sourcepp::${TARGET}c ALIAS sourcepp_${TARGET}c)
         set_target_properties(sourcepp_${TARGET}c PROPERTIES PREFIX "")
         target_link_libraries(sourcepp_${TARGET}c PRIVATE sourcepp::${TARGET})
