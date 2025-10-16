bd294b7ab47ad63f25ce18ea32f7f3b0fe26c787
cueki <github2@madisonlovett.dev>
2025-10-15 22:23:40 -0700

    mdlpp: add animation, sequence, flex, and IK support with parsing improvements

--- include/mdlpp/structs/Generic.h
+++ include/mdlpp/structs/Generic.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <string>
 
 #include <sourcepp/Macros.h>
@@ -25,8 +26,22 @@ struct BBox {
 
 struct Movement {
 	enum Flags : int32_t {
-		FLAG_NONE = 0,
-		// todo(flags): Movement
+		FLAG_NONE   = 0,
+		FLAG_X      = 0x00000001,
+		FLAG_Y      = 0x00000002,
+		FLAG_Z      = 0x00000004,
+		FLAG_XR     = 0x00000008,
+		FLAG_YR     = 0x00000010,
+		FLAG_ZR     = 0x00000020,
+		FLAG_LX     = 0x00000040,
+		FLAG_LY     = 0x00000080,
+		FLAG_LZ     = 0x00000100,
+		FLAG_LXR    = 0x00000200,
+		FLAG_LYR    = 0x00000400,
+		FLAG_LZR    = 0x00000800,
+		FLAG_LINEAR = 0x00001000,
+		FLAG_TYPES  = 0x0003FFFF,
+		FLAG_RLOOP  = 0x00040000,
 	};
 
 	int32_t endFrame;
@@ -38,5 +53,26 @@ struct Movement {
 	sourcepp::math::Vec3f relativePosition;
 };
 SOURCEPP_BITFLAGS_ENUM(Movement::Flags)
+
+struct IKLock {
+	int32_t chain;
+	float posWeight;
+	float localQWeight;
+	int32_t flags;
+
+	//int32_t unused[4];
+};
+
+union AnimValue {
+	struct {
+		uint8_t valid;
+		uint8_t total;
+	} num;
+	int16_t value;
+};
+static_assert(sizeof(AnimValue) == 2);
+
+// x/y/z or pitch/yaw/roll
+using AnimValuePtr = std::array<int16_t, 3>;
 
 } // namespace mdlpp
