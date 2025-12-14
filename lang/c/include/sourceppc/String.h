df40e540b64cae5a45cbb269dd038023c1b1b4e3
craftablescience <me@craftablescience.info>
2025-12-14 10:12:12 -0800

    c: remove existing bindings

--- lang/c/include/sourceppc/String.h
+++ lang/c/include/sourceppc/String.h
@@ -2,22 +2,24 @@
 
 #include "API.h"
 
-SOURCEPP_STRUCT_BEGIN() {
-	SOURCEPP_STRUCT_FIELD(int64_t,  size, -1);
-	SOURCEPP_STRUCT_FIELD(uint8_t*, data, NULL);
-} SOURCEPP_STRUCT_END(sourcepp, string);
+SOURCEPP_EXTERN typedef struct {
+	int64_t size;
+	char* data;
+} sourcepp_string_t;
 
 #define SOURCEPP_STRING_INVALID (SOURCEPP_CAST_CTOR(sourcepp_string_t) {.size = -1, .data = NULL})
 
-SOURCEPP_STRUCT_BEGIN() {
-	SOURCEPP_STRUCT_FIELD(int64_t, size, -1);
-	SOURCEPP_STRUCT_FIELD(char**,  data, NULL);
-} SOURCEPP_STRUCT_END(sourcepp, string_array);
+SOURCEPP_API sourcepp_string_t sourcepp_string_new(size_t size);
+
+SOURCEPP_API void sourcepp_string_free(sourcepp_string_t* str);
+
+SOURCEPP_EXTERN typedef struct {
+	int64_t size;
+	char** data;
+} sourcepp_string_array_t;
 
 #define SOURCEPP_STRING_ARRAY_INVALID (SOURCEPP_CAST_CTOR(sourcepp_string_array_t) {.size = -1, .data = NULL})
 
-SOURCEPP_STATIC(sourcepp, string, sourcepp_string_t, new, size_t size);
-SOURCEPP_STATIC(sourcepp, string,              void, free, sourcepp_string_t* str);
+SOURCEPP_API sourcepp_string_array_t sourcepp_string_array_new(size_t size);
 
-SOURCEPP_STATIC(sourcepp, string_array, sourcepp_string_array_t, new, size_t size);
-SOURCEPP_STATIC(sourcepp, string_array,                    void, free, sourcepp_string_array_t* array);
+SOURCEPP_API void sourcepp_string_array_free(sourcepp_string_array_t* array);
---
+++ lang/c/include/sourceppc/sourcepp.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "API.h"
+#include "Buffer.h"
+#include "String.h"
