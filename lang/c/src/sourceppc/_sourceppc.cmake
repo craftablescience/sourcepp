a7b530027781523f064bef87c85b27fc64d0ac8c
craftablescience <me@craftablescience.info>
2026-03-02 20:22:48 -0800

    csharp: fix compilation without C# bindings enabled, add support for using C# bindings with macOS binaries

--- lang/c/src/sourceppc/_sourceppc.cmake
+++ lang/c/src/sourceppc/_sourceppc.cmake
@@ -7,5 +7,7 @@ add_pretty_parser(${PROJECT_NAME} C
         "${CMAKE_CURRENT_LIST_DIR}/Buffer.cpp"
         "${CMAKE_CURRENT_LIST_DIR}/String.cpp")
 
-add_dependencies(sourcepp_csharp sourcepp::${PROJECT_NAME}c)
-add_custom_command(TARGET sourcepp::${PROJECT_NAME}c POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_BINARY_DIR}/sourcepp_${PROJECT_NAME}c${CMAKE_SHARED_LIBRARY_SUFFIX}" "${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/src")
+if(SOURCEPP_BUILD_CSHARP_WRAPPERS)
+    add_dependencies(sourcepp_csharp sourcepp::${PROJECT_NAME}c)
+    add_custom_command(TARGET sourcepp::${PROJECT_NAME}c POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_BINARY_DIR}/sourcepp_${PROJECT_NAME}c${CMAKE_SHARED_LIBRARY_SUFFIX}" "${CMAKE_CURRENT_SOURCE_DIR}/lang/csharp/src")
+endif()
