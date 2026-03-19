d5bffb88a52ee80c3a4b5f28563e3776c723357e
Sheepy <alabtask@gmail.com>
2026-03-18 21:49:25 -0500

    c: changed std::string_view to char* in WAD3.h

--- lang/c/include/vpkppc/format/WAD3.h
+++ lang/c/include/vpkppc/format/WAD3.h
@@ -4,7 +4,7 @@
 
 VPKPP_EXTERNVAR const int8_t VPKPP_WAD3_FILENAME_MAX_SIZE;
 VPKPP_EXTERNVAR const uint32_t VPKPP_WAD3_SIGNATURE;
-VPKPP_EXTERNVAR const std::string_view VPKPP_WAD3_EXTENSION;
+VPKPP_EXTERNVAR const char* VPKPP_WAD3_EXTENSION;
 
 VPKPP_API vpkpp_pack_file_handle_t vpkpp_wad3_create(const char* path); // REQUIRES MANUAL FREE: vpkpp_close
 VPKPP_API vpkpp_pack_file_handle_t vpkpp_wad3_open(const char* path, vpkpp_entry_callback_t callback); // REQUIRES MANUAL FREE: vpkpp_close
