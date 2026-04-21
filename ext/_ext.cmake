4888f1a403389899a83decc3edd66b57aed98d41
craftablescience <me@craftablescience.info>
2026-04-21 00:29:04 -0700

    ext: use internal setting var

--- ext/_ext.cmake
+++ ext/_ext.cmake
@@ -57,7 +57,7 @@ if(NOT TARGET MINIZIP::minizip)
     set(MZ_OPENSSL          OFF CACHE INTERNAL "")
     set(SKIP_INSTALL_ALL    ON  CACHE INTERNAL "" FORCE)
 
-    set(ZSTD_MULTITHREAD_SUPPORT ${SOURCEPP_BUILD_WITH_THREADS} CACHE BOOL "" FORCE)
+    set(ZSTD_MULTITHREAD_SUPPORT ${SOURCEPP_BUILD_WITH_THREADS} CACHE INTERNAL "" FORCE)
 
     add_sourcepp_remote_library(minizip-ng https://github.com/craftablescience/minizip-ng 2f0041b6f7c2193a06d18ca47ccd81fc7070ee8f)
 
